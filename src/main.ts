0a42e121cc129f276d57c3c187991d8bd5241f6d
Roger French <rogerf76@gmail.com>
2025-04-01 18:50:50 +1100

    Fix cors issues
    - Simplify enableCors() call
    - Add required methods to vercel.json

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-        origin: process.env.FRONTEND_URL,
-        methods: [ "GET", "POST", "PUT", "DELETE","OPTIONS", "PATCH"],
-        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-     });
+    app.enableCors();
     
     await app.listen(process.env.PORT ?? 3000);
 }
